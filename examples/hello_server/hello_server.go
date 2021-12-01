7f44496460185dfc3d3d974b8c6d513529189ddc
柴克斌 <chaikebin@corp.netease.com>
2021-12-01 15:05:00 +0800

    update hello_server

--- examples/hello_server/hello_server.go
+++ examples/hello_server/hello_server.go
@@ -15,87 +15,15 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"net/http"
-	"net/http/httptest"
-
-	"github.com/NetEase-Media/ngo/adapter/sentinel"
-
-	"github.com/NetEase-Media/ngo/adapter/log"
 	"github.com/NetEase-Media/ngo/adapter/protocol"
-	"github.com/NetEase-Media/ngo/client/httplib"
 	"github.com/NetEase-Media/ngo/server"
 	"github.com/gin-gonic/gin"
 )
 
-// go run . -c ./config.yaml
 func main() {
 	s := server.Init()
 	s.AddRoute(server.GET, "/hello", func(ctx *gin.Context) {
 		ctx.JSON(protocol.JsonBody("hello"))
 	})
-	// log.GetLogger("haha").Infof("gungungun.....")
-	// conf, err:= config.NewFromConfigFile("data.properties")
-	// if err ==nil {
-	// fmt.Println(conf)
-	// }
-
-	s.AddRoute(server.GET, "/error", func(ctx *gin.Context) {
-		err := errors.New("db error")
-		log.WithFields(
-			"route", "error",
-			"method", "get",
-		).Errorf("send error %s", err.Error())
-		ctx.JSON(protocol.ErrorJsonBody(protocol.DBError))
-	})
-
-	s.AddRoute(server.GET, "/panic", func(ctx *gin.Context) {
-		panic(errors.New("panic test"))
-	})
-
-	s.AddRoute(server.POST, "/httplib", httpGetHandler)
-
-	s.AddRoute(server.GET, "/cb", circuitbreakerHandler)
-
-	testServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(500)
-		w.Write([]byte("test response"))
-	}))
-	defer testServer.Close()
-
 	s.Start()
-}
-
-func httpGetHandler(ctx *gin.Context) {
-	_, err := httplib.Get("http://www.baidu.com").Do(context.Background())
-	if err != nil {
-		log.Errorf("http get failed: %s", err.Error())
-		ctx.JSON(protocol.ErrorJsonBody(protocol.SystemError))
-	} else {
-		ctx.JSON(protocol.JsonBody("Done!"))
-	}
-}
-
-var (
-	testServer *httptest.Server
-)
-
-func circuitbreakerHandler(ctx *gin.Context) {
-	fakeError := errors.New("fake error")
-	var res string
-	statusCode, err := httplib.Get(testServer.URL).BindString(&res).CircuitBreaker("count", func() error {
-		res = "circuitbreaker response"
-		return fakeError
-	}).Do(context.Background())
-
-	var blockErr *sentinel.BlockError
-	if errors.As(err, &blockErr) {
-		log.Errorf("circuitbreaker block error %s", err.Error())
-		log.Errorf("return my error? %v", errors.Is(err, fakeError))
-		ctx.JSON(200, res) // circuitbreaker response
-	} else {
-		ctx.JSON(statusCode, res) // test response
-	}
-
 }
