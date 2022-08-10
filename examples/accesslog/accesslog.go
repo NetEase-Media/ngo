900f3eafd99995c7029baeee1d7966933aadc315
machine3 <74903810+machine3@users.noreply.github.com>
2022-08-10 15:22:33 +0800

    chore:add accesslog demo

---
+++ examples/accesslog/accesslog.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "github.com/NetEase-Media/ngo/pkg/include"
+	"github.com/NetEase-Media/ngo/pkg/log"
+	"github.com/NetEase-Media/ngo/pkg/ngo"
+	"github.com/NetEase-Media/ngo/pkg/server/http"
+	"github.com/gin-gonic/gin"
+)
+
+// go run . -c ./app.yaml
+func main() {
+	app := ngo.Init()
+	s := http.Get()
+	s.AddRoute(http.GET, "/", func(ctx *gin.Context) {
+		log.Info("hello world!")
+		ctx.String(http.StatusOK, "hello world!")
+	})
+	app.Start()
+}
